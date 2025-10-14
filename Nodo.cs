76772431ede24381f358af7fef856cfc691953f0
Efrackzz <efraincepeda195@gmail.com>
2025-10-13 23:44:19 -0500

    Se agrega el proyecto

---
+++ Nodo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasePila
+{
+    internal class Nodo//Nodos para pilas dinamicas
+    {
+        public object Dato { get; set; }       // Valor que almacena el nodo
+        public Nodo Siguiente { get; set; }    // Enlace al siguiente nodo
+
+        public Nodo(object dato)
+        {
+            Dato = dato;
+            Siguiente = null;
+        }
+    }
+}
