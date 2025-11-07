19efb0ac1747afed8b27649b0e06b6bd38f70aa3
Efrackzz <efraincepeda195@gmail.com>
2025-11-07 07:22:15 -0600

    Add files via upload

---
+++ Parcial 2/ClaseFila/ClaseFila/Nodo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseFila
+{
+    internal class Nodo
+    {
+        public object Dato { get; set; }       // Valor almacenado
+        public Nodo Siguiente { get; set; }    // Apunta al siguiente nodo
+
+        public Nodo(object dato)
+        {
+            Dato = dato;
+            Siguiente = null;
+        }
+    }
+}
